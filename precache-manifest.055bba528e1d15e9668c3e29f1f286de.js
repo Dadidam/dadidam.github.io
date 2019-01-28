5ad4173d4909191c0c843c74f7188ac7e45383cd
Iliav <iliav@hurdit.com>
2019-01-28 17:42:29 -0500

    Added GA support

---
+++ precache-manifest.055bba528e1d15e9668c3e29f1f286de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ade9804352bb0d05b001",
+    "url": "/static/js/main.ade98043.chunk.js"
+  },
+  {
+    "revision": "b2d5d70cc248f8665b2a",
+    "url": "/static/js/1.b2d5d70c.chunk.js"
+  },
+  {
+    "revision": "ade9804352bb0d05b001",
+    "url": "/static/css/main.300ebbd6.chunk.css"
+  },
+  {
+    "revision": "b2d5d70cc248f8665b2a",
+    "url": "/static/css/1.6a8a1f1e.chunk.css"
+  },
+  {
+    "revision": "4c7dfb9dd583ec65320eccd426e560d0",
+    "url": "/index.html"
+  }
+];
